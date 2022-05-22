07260081e57b5ff3de9959106e77eff386b722e7
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-05-22 18:22:30 -0400

    commit tree eaa4b3036aab1db2e25fac07ec14785a17d31e77

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAddItem, setIsAddItem] = useState(false);
+  
+  function handleAddItemClick() {
+    setIsAddItem((isAddItem) => !isAddItem);
+  }
+
   return (
-    <li className="">
+    <li className={isAddItem ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isAddItem ? "remove" : "add"} onClick={handleAddItemClick}>{isAddItem ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
